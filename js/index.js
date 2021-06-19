07c1b446cec4f1b838cbaa96420f87270140c59c
kali <kali@kali.kali>
2021-06-19 11:52:48 +0500

    home page

--- js/index.js
+++ js/index.js
@@ -25,3 +25,31 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+var swiper = new Swiper(".mySwiper-2", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay :{
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+});
